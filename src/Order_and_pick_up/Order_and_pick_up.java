e4272d80bfcd5c9b1b132d702bc822bfdc1d26ac
Milica Djuric <milicadjuricrs@gmail.com>
2022-01-07 21:12:42 +0100

    Init project

--- src/Order_and_pick_up/Order_and_pick_up.java
+++ src/Order_and_pick_up/Order_and_pick_up.java
@@ -1,10 +1,29 @@
 package Order_and_pick_up;
 
+import java.util.Scanner;
+
 public class Order_and_pick_up {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner s=new Scanner(System.in);
+		
+		 String[] ingredients = { "Macaroni", "Spaghetti", "Bolognese", "Turkey meat", "Beef prosciutto", "Bacon",
+		"Chicken meat", "4 cheeses", "Smoked cheese", "Parmesan", "Sour cream", "Pesto sauce", "Napolitana",
+		"Vegetable mix", "Mushrooms", "Box" };
 
+		 int[] prices = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+
+		 String[] regularCustomers = { "0621122333", "0605544333", "0641234561", "0654321321", "0671234567" };
+
+	}
+	public static int findIngredient(String[] ingredients, String ingredientName) {
+		int index = 0;
+		for (int i = 0; i < ingredients.length; i++)
+			if (ingredientName.equals(ingredients[i])) {
+				return index = i;
+			}
+		return index;
 	}
 
 }
